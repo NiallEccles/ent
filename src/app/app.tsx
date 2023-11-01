9d13685e097db1f0075b93c4a33b667cba7369be
EcclesN <Niall.Eccles@hmhco.com>
2023-11-01 21:48:21 +0000

    Add tabs and coming soon tooltip

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,35 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-import { Button } from "@/components/ui/button"
+import { PrimaryNavigation } from "@/components/primary-navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export function App() {
   return (
     <div>
-      <Button>Click me</Button>
-      <NxWelcome title="ent" />
+      <PrimaryNavigation/>
+      <Tabs defaultValue="presets" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="presets">Presets</TabsTrigger>
+            <Tooltip>
+              <TooltipTrigger>
+                <TabsTrigger value="custom" disabled={true}>
+                  Custom
+                </TabsTrigger>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Coming soon!</p>
+              </TooltipContent>
+            </Tooltip>
+        </TabsList>
+        <TabsContent value="presets">Make changes to your account here.</TabsContent>
+        <TabsContent value="custom">Change your password here.</TabsContent>
+      </Tabs>
     </div>
   );
 }
